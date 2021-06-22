a5e082a43c0eda307f17bd8376f6246ec0eb419b
Fredy Nur Apriyanto <mail.fredyna@gmail.com>
2021-06-22 07:30:15 +0700

    update fitur grafik data kecelakaan

--- app/Http/Controllers/DataKecelakaanController.php
+++ app/Http/Controllers/DataKecelakaanController.php
@@ -42,7 +42,51 @@ class DataKecelakaanController extends Controller
             })
             ->orderBy('waktu_laka', 'desc')
             ->get();
+        $data['grafik_jenis_laka'] = [
+            'TABRAK DEPAN' => $this->getDataJenisLaka('TABRAK DEPAN', $first_date, $last_date),
+            'TABRAK BELAKANG' => $this->getDataJenisLaka('TABRAK BELAKANG', $first_date, $last_date),
+            'TABRAK SAMPING' => $this->getDataJenisLaka('TABRAK SAMPING', $first_date, $last_date),
+            'LAKA TUNGGAL' => $this->getDataJenisLaka('LAKA TUNGGAL', $first_date, $last_date),
+            'LAKA KRAMBOL' => $this->getDataJenisLaka('LAKA KRAMBOL', $first_date, $last_date),
+            'TABRAK LARI' => $this->getDataJenisLaka('TABRAK LARI', $first_date, $last_date),
+            'TABRAK MANUSIA' => $this->getDataJenisLaka('TABRAK MANUSIA', $first_date, $last_date),
+            'TABRAK KA' => $this->getDataJenisLaka('TABRAK KA', $first_date, $last_date)
+        ];
+        $data['grafik_sebab_laka'] = [
+            'FAKTOR MANUSIA' => $this->getDataSebabLaka('FAKTOR MANUSIA', $first_date, $last_date),
+            'FAKTOR KENDARAAN' => $this->getDataSebabLaka('FAKTOR KENDARAAN', $first_date, $last_date),
+            'FAKTOR CUACA' => $this->getDataSebabLaka('FAKTOR CUACA', $first_date, $last_date),
+            'FAKTOR JALAN' => $this->getDataSebabLaka('FAKTOR JALAN', $first_date, $last_date),
+            'LAIN-LAIN' => $this->getDataSebabLaka('LAIN-LAIN', $first_date, $last_date),
+        ];
+        $data['grafik_tkp'] = [
+            'JALAN UTAMA' => $this->getDataTKP('JALAN UTAMA', $first_date, $last_date),
+            'JALAN KOTA' => $this->getDataTKP('JALAN KOTA', $first_date, $last_date),
+            'JALAN ALTERNATIF' => $this->getDataTKP('JALAN ALTERNATIF', $first_date, $last_date),
+            'JALAN TOL' => $this->getDataTKP('JALAN TOL', $first_date, $last_date),
+        ];
         return view('data-kecelakaan.index')->with($data);
+    }
+
+    public function getDataSebabLaka($sebab_laka, $first_date, $last_date)
+    {
+        return DataKecelakaan::where('sebab_laka', $sebab_laka)
+            ->whereBetween('waktu_laka', [$first_date, $last_date])
+            ->count();
+    }
+
+    public function getDataJenisLaka($jenis_laka, $first_date, $last_date)
+    {
+        return DataKecelakaan::where('jenis_laka', $jenis_laka)
+            ->whereBetween('waktu_laka', [$first_date, $last_date])
+            ->count();
+    }
+
+    public function getDataTKP($tkp, $first_date, $last_date)
+    {
+        return DataKecelakaan::where('tkp', $tkp)
+            ->whereBetween('waktu_laka', [$first_date, $last_date])
+            ->count();
     }
 
     public function create()
--- resources/views/data-kecelakaan/index.blade.php
+++ resources/views/data-kecelakaan/index.blade.php
@@ -17,6 +17,7 @@
             </div>
         </div>
 
+        {{-- filter data --}}
         <div class="row">
 
             <div class="col-12 grid-margin">
@@ -115,7 +116,218 @@
                 </div>
             </div>
         </div>
+        {{-- end filter data  --}}
 
+        {{-- grafik jenis laka --}}
+        <div class="row">
+            <div class="col-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <h6 class="card-title">Grafik By Jenis Laka</h6>
+                        </div>
+                        <div class="col-6 text-right">
+                            <a class="btn btn-primary btn-sm" data-toggle="collapse" href="#grafik_jenis_laka" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="grafik_jenis_laka" class="collapse">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <canvas id="chart-jenis-laka" style="height:250px"></canvas>
+                            </div>
+                            <div class="col-md-4">
+                                <table class="table table-hover table-striped">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>Jenis Laka</th>
+                                            <th class="text-center">Jumlah</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>TABRAK DEPAN</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['TABRAK DEPAN'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>TABRAK BELAKANG</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['TABRAK BELAKANG'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>TABRAK SAMPING</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['TABRAK SAMPING'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>LAKA TUNGGAL</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['LAKA TUNGGAL'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>LAKA KRAMBOL</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['LAKA KRAMBOL'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>TABRAK LARI</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['TABRAK LARI'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>TABRAK MANUSIA</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['TABRAK MANUSIA'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>TABRAK KA</td>
+                                            <td class="text-right">{{ $grafik_jenis_laka['TABRAK KA'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Total Laporan</b></td>
+                                            @php
+                                                $total_jenis_laka = $grafik_jenis_laka['TABRAK DEPAN'] + $grafik_jenis_laka['TABRAK BELAKANG'] + $grafik_jenis_laka['TABRAK SAMPING'] + $grafik_jenis_laka['LAKA TUNGGAL'] + $grafik_jenis_laka['LAKA KRAMBOL'] + $grafik_jenis_laka['TABRAK LARI'] + $grafik_jenis_laka['TABRAK MANUSIA'] + $grafik_jenis_laka['TABRAK KA'];
+                                            @endphp
+                                            <td class="text-right">{{ $total_jenis_laka }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        {{-- end grafik jenis laka --}}
+
+        {{-- grafik sebab laka --}}
+        <div class="row">
+            <div class="col-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <h6 class="card-title">Grafik By Sebab Laka</h6>
+                        </div>
+                        <div class="col-6 text-right">
+                            <a class="btn btn-primary btn-sm" data-toggle="collapse" href="#grafik_sebab_laka" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="grafik_sebab_laka" class="collapse">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <canvas id="chart-sebab-laka" style="height:250px"></canvas>
+                            </div>
+                            <div class="col-md-4">
+                                <table class="table table-hover table-striped">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>Sebab Laka</th>
+                                            <th class="text-center">Jumlah</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>FAKTOR MANUSIA</td>
+                                            <td class="text-right">{{ $grafik_sebab_laka['FAKTOR MANUSIA'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>FAKTOR KENDARAAN</td>
+                                            <td class="text-right">{{ $grafik_sebab_laka['FAKTOR KENDARAAN'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>FAKTOR CUACA</td>
+                                            <td class="text-right">{{ $grafik_sebab_laka['FAKTOR CUACA'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>FAKTOR JALAN</td>
+                                            <td class="text-right">{{ $grafik_sebab_laka['FAKTOR JALAN'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>LAIN-LAIN</td>
+                                            <td class="text-right">{{ $grafik_sebab_laka['LAIN-LAIN'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Total Laporan</b></td>
+                                            @php
+                                                $total_sebab_laka = $grafik_sebab_laka['FAKTOR MANUSIA'] + $grafik_sebab_laka['FAKTOR KENDARAAN'] + $grafik_sebab_laka['FAKTOR CUACA'] + $grafik_sebab_laka['FAKTOR JALAN'] + $grafik_sebab_laka['LAIN-LAIN'];
+                                            @endphp
+                                            <td class="text-right">{{ $total_sebab_laka }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        {{-- end grafik sebab laka --}}
+
+        {{-- grafik tkp --}}
+        <div class="row">
+            <div class="col-12 grid-margin stretch-card">
+                <div class="card">
+                  <div class="card-body">
+                    <div class="row">
+                        <div class="col-6">
+                            <h6 class="card-title">Grafik By TKP</h6>
+                        </div>
+                        <div class="col-6 text-right">
+                            <a class="btn btn-primary btn-sm" data-toggle="collapse" href="#grafik_tkp" role="button" aria-expanded="false" aria-controls="collapseExample">
+                                <i class="fa fa-chevron-down"></i>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="grafik_tkp" class="collapse">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <canvas id="chart-tkp" style="height:250px"></canvas>
+                            </div>
+                            <div class="col-md-4">
+                                <table class="table table-hover table-striped">
+                                    <thead class="thead-dark">
+                                        <tr>
+                                            <th>TKP</th>
+                                            <th class="text-center">Jumlah</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>JALAN UTAMA</td>
+                                            <td class="text-right">{{ $grafik_tkp['JALAN UTAMA'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>JALAN KOTA</td>
+                                            <td class="text-right">{{ $grafik_tkp['JALAN KOTA'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>JALAN ALTERNATIF</td>
+                                            <td class="text-right">{{ $grafik_tkp['JALAN ALTERNATIF'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>JALAN TOL</td>
+                                            <td class="text-right">{{ $grafik_tkp['JALAN TOL'] }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td><b>Total Laporan</b></td>
+                                            @php
+                                                $total_tkp = $grafik_tkp['JALAN UTAMA'] + $grafik_tkp['JALAN KOTA'] + $grafik_tkp['JALAN ALTERNATIF'] + $grafik_tkp['JALAN TOL'];
+                                            @endphp
+                                            <td class="text-right">{{ $total_tkp }}</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        {{-- end grafik sebab laka --}}
+
+        {{-- tabel data  --}}
         <div class="row">
 
             <div class="col-12 grid-margin">
@@ -128,7 +340,7 @@
                             @php
                                 $no = 1;
                             @endphp
-                            <table id="table-data" class="table table-striped table-hover" width="100%">
+                            <table id="{{ count($kecelakaan) > 0 ? 'table-data':'' }}" class="table table-striped table-hover" width="100%">
                                 <thead class="thead-dark">
                                     <tr>
                                         <th>#</th>
@@ -180,16 +392,153 @@
                 </div>
             </div>
         </div>
+        {{-- end tabel data  --}}
     </div>
     <!-- content-wrapper ends -->
 @endsection
 
 @section('js')
+    <script src="{{ asset('assets/js/dashboard.js')}}"></script>
     <script>
         $(function(){
             $("#data-kecelakaan").addClass('active');
             $("#table-data").dataTable();
         });
+
+        // chart by jenis laka
+        let chartJenisData = {
+            datasets: [{
+                data: [
+                    {{ $grafik_jenis_laka['TABRAK DEPAN'] }}, {{ $grafik_jenis_laka['TABRAK BELAKANG'] }},
+                    {{ $grafik_jenis_laka['TABRAK SAMPING'] }}, {{ $grafik_jenis_laka['LAKA TUNGGAL'] }},
+                    {{ $grafik_jenis_laka['LAKA KRAMBOL'] }}, {{ $grafik_jenis_laka['TABRAK LARI'] }},
+                    {{ $grafik_jenis_laka['TABRAK MANUSIA'] }}, {{ $grafik_jenis_laka['TABRAK KA'] }}
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                    'rgba(153, 51, 153, 0.5)',
+                    'rgba(51, 51, 204, 0.5)',
+                    'rgba(0, 255, 153, 0.5)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(153, 51, 153, 1)',
+                    'rgba(51, 51, 204, 1)',
+                    'rgba(0, 255, 153, 1)'
+                ],
+            }],
+            labels: [
+                    'TABRAK DEPAN', 'TABRAK BELAKANG', 'TABRAK SAMPING', 'LAKA TUNGGAL',
+                    'LAKA KRAMBOL', 'TABRAK LARI', 'TABRAK MANUSIA', 'TABRAK KA'
+                ]
+        };
+        let doughnutJenisLaka = {
+            responsive: true,
+            animation: {
+            animateScale: true,
+            animateRotate: true
+            }
+        };
+
+        let chartJenisCanvas = $("#chart-jenis-laka").get(0).getContext("2d");
+        let chartJenisLaka = new Chart(chartJenisCanvas, {
+            type: 'doughnut',
+            data: chartJenisData,
+            options: doughnutJenisLaka
+        });
+        //end chart
+
+        // chart by sebab laka
+        let chartSebabData = {
+            datasets: [{
+                data: [
+                    {{ $grafik_sebab_laka['FAKTOR MANUSIA'] }}, {{ $grafik_sebab_laka['FAKTOR KENDARAAN'] }},
+                    {{ $grafik_sebab_laka['FAKTOR CUACA'] }}, {{ $grafik_sebab_laka['FAKTOR JALAN'] }},
+                    {{ $grafik_sebab_laka['LAIN-LAIN'] }}
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                    'rgba(153, 102, 255, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                ],
+            }],
+            labels: [
+                    'FAKTOR MANUSIA', 'FAKTOR KENDARAAN', 'FAKTOR CUACA', 'FAKTOR JALAN',
+                    'LAIN-LAIN'
+                ]
+        };
+        let doughnutSebabLaka = {
+            responsive: true,
+            animation: {
+            animateScale: true,
+            animateRotate: true
+            }
+        };
+
+        let chartSebabCanvas = $("#chart-sebab-laka").get(0).getContext("2d");
+        let chartSebabLaka = new Chart(chartSebabCanvas, {
+            type: 'doughnut',
+            data: chartSebabData,
+            options: doughnutSebabLaka
+        });
+        //end chart
+
+        // chart by tkp
+        let chartTKPData = {
+            datasets: [{
+                data: [
+                    {{ $grafik_tkp['JALAN UTAMA'] }}, {{ $grafik_tkp['JALAN KOTA'] }},
+                    {{ $grafik_tkp['JALAN ALTERNATIF'] }}, {{ $grafik_tkp['JALAN TOL'] }}
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.5)',
+                    'rgba(54, 162, 235, 0.5)',
+                    'rgba(255, 206, 86, 0.5)',
+                    'rgba(75, 192, 192, 0.5)',
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                ],
+            }],
+            labels: [
+                    'JALAN UTAMA', 'JALAN KOTA', 'JALAN ALTERNATIF', 'JALAN TOL'
+                ]
+        };
+        let doughnutTKP = {
+            responsive: true,
+            animation: {
+            animateScale: true,
+            animateRotate: true
+            }
+        };
+
+        let chartTKPCanvas = $("#chart-tkp").get(0).getContext("2d");
+        let chartTKP = new Chart(chartTKPCanvas, {
+            type: 'doughnut',
+            data: chartTKPData,
+            options: doughnutTKP
+        });
+        //end chart
 
         function hapusData(id){
           let y = confirm('Yakin mau dihapus?');
